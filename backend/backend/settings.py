21ac3545620130db295818273c3bed3ba6561876
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-09-26 15:00:01 +0300

    Serve React app through Django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-_xr8dz81kb)f96ruk2q_f%m@zbkt-=1)q$elp4hi4)=^gh=5pf')
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS configuration
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
