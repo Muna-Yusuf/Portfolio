2e16202f6d0331d6cf840e8d9778746038798581
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-09-26 20:28:48 +0300

    update req

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('contact.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
-]
+    ]
